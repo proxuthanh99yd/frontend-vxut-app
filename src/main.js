498a2db6fee8bed7b11098614119dccbbb52fa1c
proxuthanh99yd <proxuthanh99yd@gmail.com>
2022-02-07 12:11:47 +0700

    .env 	src/assets/_global.scss 	src/assets/icons/ 	src/assets/js/ 	src/assets/scss/ 	src/components/ 	src/middleware/ 	src/mixins/ 	src/pages/ 	src/router/ 	src/service/ 	src/store/ 	vue.config.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,67 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
+import "./assets/_global.scss";
+import router from "./router/routes";
+import VueRouter from "vue-router";
+import store from "./store/index";
+import VueFormulate from '@braid/vue-formulate'
+import vSelect from 'vue-select'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from "axios";
+import { BootstrapVueIcons } from 'bootstrap-vue';
+import VueCountdownTimer from 'vuejs-countdown-timer';
+import CKEditor from 'ckeditor4-vue';
+
+Vue.use(CKEditor);
+Vue.use(VueCountdownTimer);
+
+Vue.use(BootstrapVueIcons)
+import locale from 'element-ui/lib/locale/lang/en'
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response.status === 422) {
+      store.commit("setErrors", error.response.data.errors);
+    } else if (error.response.status === 401) {
+      store.commit("auth/setUserData", null);
+      localStorage.removeItem("authToken");
+      router.push({ name: "Login" });
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+axios.interceptors.request.use(function (config) {
+  config.headers.common = {
+    Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  };
+
+  return config;
+});
+
+
+Vue.use(ElementUI, { locale });
+
+
+Vue.component('v-select', vSelect)
+Vue.use(VueFormulate)
+Vue.use(VueRouter);
+
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+
+
 
 Vue.config.productionTip = false
-
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
